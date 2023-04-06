7753e644dc4ddb07863ec6f7ffd6325d6dd8f8fa
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-04-06 23:04:04 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- Script that creates a table users
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
+-- Creates a table users
+-- Script should be executed on any database
+CREATE TABLE IF NOT EXISTS users(
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	)
