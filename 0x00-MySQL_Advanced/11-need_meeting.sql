544aaba4c08be88bf57f8ad33da2ea01f6bbcfee
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-04-06 23:49:06 +0100

    SQL!

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,10 +2,7 @@
 -- students that have a score under 80 (strict) and no last_meeting or
 -- more than 1 month.
 
--- Requirements:
--- The view need_meeting should return all students name when:
--- They score are under (strict) to 80
--- AND no last_meeting date OR more than a month
+
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name
