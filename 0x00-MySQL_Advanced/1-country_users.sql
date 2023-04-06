bd66c80953612a7eae8eaa9ae5dd7d0c93260702
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-04-06 23:28:40 +0100

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Creates a table users that takes in enumeration of countries
 -- Script should be executed on any database
-CREATE TABLE IF NOT EXISTS users(
-	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-	)
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
